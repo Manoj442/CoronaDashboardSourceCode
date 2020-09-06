56df0b7ca154f879a389ec4de627c2bd852e8472
Manoj <manoj.gudivada1@gmail.com>
2020-09-06 12:09:10 +0530

    first commit

---
+++ build/precache-manifest.5625978c809b4125225467058818078f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc3be3713d64048eee07c00b9eaf809",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "38d0420751075d937d7e",
+    "url": "/CoronaTracker/static/css/main.87d66f1f.chunk.css"
+  },
+  {
+    "revision": "236d59dab2cd2c3867e9",
+    "url": "/CoronaTracker/static/js/2.10180915.chunk.js"
+  },
+  {
+    "revision": "953ddbfd33e29cfe417b4c1451a30c50",
+    "url": "/CoronaTracker/static/js/2.10180915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d0420751075d937d7e",
+    "url": "/CoronaTracker/static/js/main.17385863.chunk.js"
+  },
+  {
+    "revision": "597c64ed36e2afa5fb37",
+    "url": "/CoronaTracker/static/js/runtime-main.c2155eb9.js"
+  },
+  {
+    "revision": "e5a2ad75b819a5dfefeddb05151d4b0c",
+    "url": "/CoronaTracker/static/media/covid19.e5a2ad75.jpg"
+  },
+  {
+    "revision": "7f263f203d2a38809ca18e8307e2c710",
+    "url": "/CoronaTracker/static/media/india.7f263f20.jpg"
+  }
+]);
